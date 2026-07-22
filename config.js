546b323b17977e8d98804cc728b3da3b0e5addfe
Saink07 <omsaink07@gmail.com>
2026-07-22 22:29:21 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+SERVER: {  
+    PORT: process.env.PORT || 3000  
+},  
+
+PATH: {  
+
+    VILLAGES: "./data/villages.json"  
+
+},  
+
+API: {  
+
+    VERSION: "v1"  
+
+}
+
+};
